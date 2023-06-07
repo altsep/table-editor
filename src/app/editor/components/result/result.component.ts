f5854b16dcc047e97777f9b1785d9b690d7d4c0d
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-06-07 12:40:17 +0300

    improvement: ensure items can be unloaded after simply switching mode
    without updating any cells

--- src/app/editor/components/result/result.component.ts
+++ src/app/editor/components/result/result.component.ts
@@ -2,22 +2,25 @@ import { Component } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { TableItems } from '../../models/table.model';
 import { CsvPipe } from '../../pipes/csv.pipe';
+import { ItemsPipe } from '../../pipes/items.pipe';
 import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss'],
-  providers: [CsvPipe],
+  providers: [ItemsPipe, CsvPipe],
 })
 export class ResultComponent {
   public items?: TableItems;
 
-  public data?: string;
-
-  constructor(private dataService: DataService, private csvPipe: CsvPipe) {
+  constructor(private dataService: DataService, itemsPipe: ItemsPipe, private csvPipe: CsvPipe) {
     dataService.data$.pipe(takeUntilDestroyed()).subscribe((data) => {
-      this.data = data;
+      const items = itemsPipe.transform(data);
+
+      if (items != null) {
+        this.items = items;
+      }
     });
   }
 
