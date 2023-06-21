ffa88e70a8b0a038ce5bbeee11a9d846f97fd28b
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-06-21 15:15:36 +0300

    refactor: use OnChanges for updating input validators
    clear selected file on mode change

--- src/app/editor/components/input-file/input-file.component.ts
+++ src/app/editor/components/input-file/input-file.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { DataType } from '../../types/dataFormat.type';
 
 @Component({
   selector: 'app-input-file',
@@ -7,6 +8,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class InputFileComponent {
   @Output() public dataChange = new EventEmitter<string>();
+
+  @ViewChild('input') public inputRef!: ElementRef<HTMLInputElement>;
+
+  @Input() public set dataType(_: DataType) {
+    if (this.inputRef != null) {
+      this.inputRef.nativeElement.value = '';
+    }
+  }
 
   public async onChange(e: Event): Promise<void> {
     const target = e.target as HTMLInputElement;
--- src/app/editor/components/input/input.component.ts
+++ src/app/editor/components/input/input.component.ts
@@ -1,15 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { DataType } from '../../types/dataFormat.type';
-import { jsonValidator } from '../../validators/json-validator';
 import { csvValidator } from '../../validators/csv-validator';
+import { jsonValidator } from '../../validators/json-validator';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
 })
-export class InputComponent {
+export class InputComponent implements OnChanges {
   public control = new FormControl('', [Validators.required]);
 
   public get data(): string {
@@ -20,20 +20,17 @@ export class InputComponent {
     this.control.setValue(value);
   }
 
-  private _dataType!: DataType;
-
-  public get dataType(): DataType {
-    return this._dataType;
-  }
-
-  @Input() public set dataType(value: DataType) {
-    this._dataType = value;
-    this.updateValidators();
-  }
-
   @Output() public dataChange = new EventEmitter<string>();
 
+  @Input() public dataType!: DataType;
+
   @Output() public dataTypeChange = new EventEmitter<DataType>();
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['dataType']) {
+      this.updateValidators();
+    }
+  }
 
   public load(): void {
     this.dataChange.emit(this.data);
@@ -41,11 +38,11 @@ export class InputComponent {
 
   private updateValidators(): void {
     const customValidators = {
-      json: jsonValidator(),
-      csv: csvValidator(),
+      json: jsonValidator,
+      csv: csvValidator,
     };
     const inputValidator = customValidators[this.dataType];
-    this.control.setValidators([Validators.required, inputValidator]);
+    this.control.setValidators([Validators.required, inputValidator()]);
     this.control.updateValueAndValidity();
   }
 }
